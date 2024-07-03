8b9ad7d510f05cec5a373145bb7ad8209b6c9ca4
sbin.jeon <sbin.jeon@samsung.com>
2024-07-03 11:09:47 +0900

    [refactor] 38p Apply TestFixture

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -3,14 +3,18 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
+class PrimeTexture : public testing::Test {
+public:
 	PrimeFactor pf;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeTexture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, pf.of(1));
 }
-TEST(PrimeFactors, Of2) {
-	PrimeFactor pf;
-	vector<int> expected = {2};
+TEST_F(PrimeTexture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, pf.of(2));
 }
 
